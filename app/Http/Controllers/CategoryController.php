ad2978821e2cff01e31c733fd74108a61ce474e1
abichesta1102 <abichesta1102@gmail.com>
2026-09-03 10:35:49 +0700

    [P-2] Selesai konfigurasi controller, route, dan database

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+// CategoryController dianalogikan seperti seorang pelayan yang bertugas mengatur dan melayani segala urusan kategori buku.
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index() { return 'CategoryController@index'; }
+    public function create() { return 'CategoryController@create'; }
+    public function store(Request $request) { return 'CategoryController@store'; }
+    public function edit(string $id) { return "CategoryController@edit, id: {$id}"; }
+    public function update(Request $request, string $id) { return "CategoryController@update, id: {$id}"; }
+    public function destroy(string $id) { return "CategoryController@destroy, id: {$id}"; }
+}
